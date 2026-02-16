3f0a2037d5ff1db7ca5805b2845008f05315e21c
Chris Coutts <ccoutts@mmgy.com>
2026-02-16 10:55:27 +0000

    Complete Shopify developer test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,29 @@ console.log('Shopify Liquid Test - Ready!');
 
 // Example: Add interactivity if needed
 document.addEventListener('DOMContentLoaded', () => {
-  // Your interactive code here
+  
+  const products = document.querySelectorAll('.ProductCard');
+
+  products.forEach((product) => {
+    const minusQuantityButton = product.querySelector('#minus-quantity');
+    const addQuantityButton = product.querySelector('#add-quantity');
+    const quantityAmount = product.querySelector('#quantity');
+    const quantityAmountValue = product.querySelector('.Product__quantityValue');
+
+    let count = 1;
+
+    quantityAmount.textContent = count;
+
+    minusQuantityButton.addEventListener("click", () => {
+      if (count > 1) {
+        quantityAmount.textContent = count -= 1;
+        quantityAmountValue.value = count;
+      }
+    })
+
+    addQuantityButton.addEventListener("click", () => {
+     quantityAmount.textContent = count += 1;
+     quantityAmountValue.value = count;
+    })
+  })
 });
